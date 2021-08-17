5d09bbc0365c03465fb587269e4d5154b06e6f8e
jack <jack@atguigu.com>
2021-08-17 09:41:17 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author niyinghao
+ * @create 2021-08-17 9:19
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
